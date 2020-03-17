f680fe576a0bfaf41c607a01d2338e48d8829f0f
annaabelman <aabelman@ucsb.edu>
2020-03-17 16:53:39 -0700

    added source information

--- R/pops_sizes.R
+++ R/pops_sizes.R
@@ -1,7 +1,7 @@
 #' @title Population Size and Location Data Frame
 #' @description A sample data frames created that includes vectors of: population, location, and size of location in kilometers.
 #' @format Individual vectors with list of each variable were initially created. A data frame was created in tidy format.
-#' @source This data was randomly created
+#' @source his data was randomly created, but inspired by the dataset provided by the SSTI analysis of BEA and Census Data from 2015 on GDP per county in the USA.
 #' \describe{
 #' \population - a list of the population sizes for each location
 #' \locs - a list of 10 different California locations
